92300de95354ff2973db2da2dbcee6bd0c5ff3a7
Satwik <bandasatwik4@gmail.com>
2024-03-02 10:58:37 +0530

    bg added to auth pages

--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -3,12 +3,11 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import studentImage from "../../../assets/Signup/student.png";
-import adminImage from "../../../assets/Signup/admin.jpg";
+import adminImage from "../../../assets/Signup/admin.png";
 
 const SignUp = () => {
-  const navbarHeight = 60;
   return (
-    <div className="h-[screen-20] flex flex-col bg-gradient-to-t from-blue-300 via-pink-200 to-blue-100 p-10 px-5 ">
+    <div className="h-screen flex flex-col bg-gradient-to-t from-blue-300 via-pink-200 to-blue-100 p-10 px-5 ">
       <div className=" flex flex-col mb-5  p-10 px-5 ">
         <h1 className="mb-2 text-[32px] md:text-[40px] lg:text-[48px] xl:text-[48px] font-extrabold text-dark-grey-900 text-center">
           Sign Up
