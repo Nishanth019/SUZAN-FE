c0d56fd8c4b62fa559b70ad36997a98585af7854
Nishanth019 <bhukyanishanth69@gmail.com>
2024-03-09 03:13:56 +0530

    create authContext

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ClientProvider from "@/components/ClientProvider/ClientProvider";
+import { AuthProvider } from '@/context/AuthContext'; // Import the context provider
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className} suppressHydrationWarning>
+      <AuthProvider>
         <ClientProvider>{children}</ClientProvider>
+        </AuthProvider>
       </body>
     </html>
   );
---
+++ src/context/AuthContext.js
@@ -0,0 +1,41 @@
+'use client'
+import { createContext, useContext, useState } from 'react';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+    const [isAuth, setIsAuth] = useState(false);
+    const [user, setUser] = useState(null)
+
+    useEffect(()=>{
+        const getCurrentUser = async()=>{
+          const currentUser = await AuthService.getCurrentUser();
+          if(currentUser){
+            setIsAuth(true);
+            setUser(currentUser);
+          }
+          else{
+            setIsAuth(false);
+            setUser(null);
+          }
+        }
+        getCurrentUser();
+    },[])
+
+  return (
+    <AuthContext.Provider
+     value={{ 
+        isAuth,
+        setIsAuth, 
+        user,
+        setuser,
+       }}
+     >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AuthContext);
+};
