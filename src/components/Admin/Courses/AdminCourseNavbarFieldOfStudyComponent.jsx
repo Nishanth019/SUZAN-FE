b603efd3d3321fff9dff3b8b9197f1551270e830
Satwik <bandasatwik4@gmail.com>
2024-04-23 01:20:29 +0530

    FOS changes1

--- src/components/Admin/Courses/AdminCourseNavbarFieldOfStudyComponent.jsx
+++ src/components/Admin/Courses/AdminCourseNavbarFieldOfStudyComponent.jsx
@@ -1,6 +1,7 @@
 "use client";
 import Dropdown1 from "@/components/TailwindComponents/Dropdown";
 import Dropdown2 from "@/components/TailwindComponents/FormDropdown";
+import Dropdown from "@/components/TailwindComponents/Dropdown";
 import { FaSearch } from "react-icons/fa";
 import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
@@ -24,55 +25,190 @@ const style = {
 
 const AdminCourseNavbarFieldOfStudyComponent = () => {
   const [selectedProgram, setSelectedProgram] = useState("");
+  const [selectedProgramDetails, setSelectedProgramDetails] = useState("");
   const [selectedFieldOfStudy, setSelectedFieldOfStudy] = useState("");
-  const [programs, setPrograms] = useState([]); // State to store programs
-  const [fieldOfStudy, setFieldOfStudy] = useState([]); // State to store programs
+  const [courseCount, setCourseCount] = useState(0);
+  // const [openModal, setOpenModal] = useState(false);
+  // const [selectedSemester, setSelectedSemester] = useState("");
+  // const [searchQuery, setSearchQuery] = useState("");
 
-  //div useStates
-  const [fos, setFos] = useState("");
-  const [fullFos, setFullFos] = useState("");
-  const [formselectedProgram, setFormSelectedProgram] = useState("");
-  const [openModal, setOpenModal] = useState(false);
+  // Selected object values
+  const [programs, setPrograms] = useState([]);
+  const [fieldOfStudy, setFieldOfStudy] = useState([]);
+  const [currentFieldOfStudy, setCurrentFieldOfStudy] = useState(null);
+  // const [semesters, setSemesters] = useState([]);
+  // const [courses, setCourses] = useState([]);
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
 
-  const [fosDetails, setFosDetails] = useState({
-    fieldsOfStudy_name: "",
-    fieldsOfStudy_full_name: "",
-  });
+
+
 
   useEffect(() => {
-    // Fetch all programs when the component mounts
-    fetchAllFieldOfStudy();
-    fetchAllProgram();
-  }, []);
-  // useEffect(() => {
-  //   console.log("ifi", programs)
-  // }, [programs]);
-  // Function to fetch all fieldofstudy
-  const fetchAllFieldOfStudy = async () => {
-    try {
-      const response = await CourseService.getAllFieldsOfStudy();
-      setFieldOfStudy(response.data.programs);
-    } catch (error) {
-      console.error("Error fetching fos:", error);
+    // Fetch all programs on component mount
+    async function fetchPrograms() {
+      try {
+        const response = await CourseService.getAllPrograms();
+        setPrograms(response.data.programs);
+        // setSelectedProgram(response.data.programs[0]?._id);
+      } catch (error) {
+        console.error("Error fetching programs:", error);
+      }
     }
-  };
-  const fetchAllProgram = async () => {
+    fetchPrograms();
+  }, []);
+
+  
+  useEffect(() => {
+    // Fetch fields of study when program selected
+    async function fetchFieldsOfStudy(programId) {
+      try {
+        const response = await CourseService.getAllFieldsOfStudy(programId);
+        setFieldOfStudy(response.data.fieldsOfStudy);
+        // setSelectedFieldOfStudy(response.data.fieldsOfStudy[0]?._id);
+      } catch (error) {
+        console.error("Error fetching fields of studys:", error);
+      }
+    }
+    if (selectedProgram) {
+      fetchFieldsOfStudy(selectedProgram);
+    }
+    async function fetchProgramDetailsById(programId) {
+      try {
+        const response = await CourseService.getProgramById(programId);
+        console.log(12345,response.data)
+        setSelectedProgramDetails(response.data.program);
+      } catch (error) {
+        console.error("Error fetching program:", error);
+      }
+    }
+    if(selectedProgram){
+      fetchProgramDetailsById(selectedProgram)
+    }
+  }, [selectedProgram]);
+
+
+  useEffect(() => {
+    //function to get all field of study for a program
+    async function fetchFieldOfStudy() {
+      try {
+        console.log(1223456)
+        const response = await CourseService.getAllFieldsOfStudy(selectedProgram);
+        console.log("1054",response.data);
+        const fieldOfStudyWithCounts = await Promise.all(
+          response.data.fieldsOfStudy.map(async (fieldofstudy) => {
+            // Fetch the count of courses for each program
+            const coursesCount = await fetchCourseCountForFieldOfStudy(selectedProgram,fieldofstudy._id);
+            // Fetch the count of field of study for each program
+            const fieldOfStudyCount = await fetchFieldOfStudyCountForProgram(
+              fieldofstudy._id
+            );
+            // Merge the counts with the program object
+            return { ...fieldofstudy, coursesCount, fieldOfStudyCount };
+          })
+        );
+        console.log(69,fieldOfStudyWithCounts);
+        setCourseCount(fieldOfStudyWithCounts);
+
+      } catch (error) {
+        console.error("Error fetching courses:", error);
+      }
+    }
+    fetchFieldOfStudy();
+
+  }, [selectedProgram]);
+
+
+
+  useEffect(() => {
+    console.log("dayday", fieldOfStudy)
+    console.log("dayday", fieldOfStudy[0])
+  }, [fieldOfStudy, selectedFieldOfStudy]);
+
+
+  useEffect(() => {
+    // Fetch courses when program, field of study, or semester selected
+    // async function fetchFieldOfStudy() {
+    //   try {
+    //     console.log(1223456)
+    //     const response = await CourseService.getAllFieldsOfStudy({programId:selectedProgram});
+    //     console.log(5,response.data);
+    //     console.log(1223455)
+    //     setFieldOfStudy(response.data.courses);
+    //   } catch (error) {
+    //     console.error("Error fetching courses:", error);
+    //   }
+    // }
+    // fetchFieldOfStudy();
+  }, [selectedProgram, selectedFieldOfStudy]);
+
+
+  const fetchCourseCountForFieldOfStudy = async (programId,fieldOfStudyId) => {
     try {
-      const response = await CourseService.getAllPrograms();
-      setPrograms(response.data.programs);
+      console.log(123456);
+      const response = await CourseService.getAllCourses(programId,fieldOfStudyId);
+      console.log(123, response);
+      return response.data.courses.length;
     } catch (error) {
-      console.error("Error fetching programs:", error);
+      console.error("Error fetching course count for program:", error);
+      return 0; // Return 0 if there's an error
     }
   };
 
   // const fieldsOfStudy = ["CSE", "ECE"];
 
   // State for modal
-  const [modalOpen, setModalOpen] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
 
   // Function to handle opening the modal
   const openAddFosModal = () => {
     setModalOpen(true);
+  };
+
+// Function to handle opening the modal for editing a course
+  const openEditfosModal = (fos) => {
+    // setCurrentfos(fos);
+    // setfosName(fos.programName);
+    // setfosFullName(fos.programFullName);
+    // setSemetersCount(fos.semestersCount);
+    setModalOpen(true);
+  };
+
+  const openDeleteModal = () => {
+    setDeleteModalOpen(true);
+  };
+
+  // Function to handle closing the delete confirmation modal
+  const closeDeleteModal = () => {
+    setDeleteModalOpen(false);
+  };
+
+  const openAddProgramModal = () => {
+    setCurrentProgram(null);
+    setProgramName("");
+    setProgramFullName("");
+    setSemetersCount("");
+    setModalOpen(true);
+  };
+  const openEditProgramModal = (program) => {
+    setCurrentProgram(program);
+    setProgramName(program.programName);
+    setProgramFullName(program.programFullName);
+    setSemetersCount(program.semestersCount);
+    setModalOpen(true);
+  };
+
+  const handleDeleteFieldOfStudy = async () => {
+    try {
+      console.log("deleting",1)
+      await CourseService.deleteFieldOfStudy(currentFieldOfStudy._id);
+      console.log("deleting",2)
+      // Refetch all fields of study after deleting
+      fetchFieldsOfStudy();
+      // Close the delete confirmation modal after deleting the program
+      closeDeleteModal();
+    } catch (error) {
+      console.error("Error deleting program:", error);
+    }
   };
 
   // Function to handle closing the modal
@@ -85,8 +221,8 @@ const AdminCourseNavbarFieldOfStudyComponent = () => {
       // Create field of study with provided data
       await CourseService.createFieldOfStudy({
         programName: selectedProgram,
-        fieldName: fosDetails.fieldsOfStudy_name,
-        fieldFullName: fosDetails.fieldsOfStudy_full_name,
+        fieldName: fieldOfStudy.fieldsOfStudy_name,
+        fieldFullName: fieldOfStudy.fieldsOfStudy_full_name,
       });
       // Close the modal after adding the field of study
       closeModal();
@@ -95,27 +231,13 @@ const AdminCourseNavbarFieldOfStudyComponent = () => {
     }
   };
 
-  const fetchCourseDetails = async () => {
-    try {
-      // Make an API request to fetch course details by ID from the backend
-      // const response = await axios.get("/api/courses/:id"); // Replace ":id" with actual course ID
-      // const fetchedCourseDetails = response.data;
-      const fetchedFosDetails = {
-        fieldsOfStudy_name: "testing ",
-        fieldsOfStudy_full_name: "testing 1",
-      };
-      // Set the course details state with the fetched data
-      setFosDetails(fetchedFosDetails);
-    } catch (error) {
-      console.error("Error fetching course details:", error);
-    }
-  };
+
 
   return (
     <div>
       <div className="flex justify-end items-center pb-2 md:pb-5">
         <button
-          onClick={openModal}
+          onClick={openAddProgramModal}
           type="button"
           className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-3 py-2 md:px-5 md:py-2.5"
         >
@@ -123,64 +245,52 @@ const AdminCourseNavbarFieldOfStudyComponent = () => {
         </button>
       </div>
       <div className="flex flex-wrap gap-2 sm:gap-5 ">
-        <Dropdown1
+        <Dropdown
           name="Program"
-          options={programs?.map((p) => p.program_name)}
-          onSelect={setSelectedProgram}
+          options={programs?.map((program) => program?.program_name)}
+          onSelect={(selectedProgramName) => {
+            const selectedProgram = programs?.find(
+              (program) => program?.program_name === selectedProgramName
+            );
+            setSelectedProgram(selectedProgram?._id);
+            // setSelectedSemester("")
+            setSelectedFieldOfStudy("")
+          }}
         />
-        <Dropdown1
+        <Dropdown
           name="Field Of Study"
-          options={fieldOfStudy?.map((p) => p.fieldsOfStudy_name)}
-          onSelect={setSelectedFieldOfStudy}
+          options={fieldOfStudy?.map((field) => field?.field_of_studyname)}
+          onSelect={(selectedFieldOfStudyName) => {
+            const selectedFieldOfStudy = fieldOfStudy?.find(
+              (field) => field?.field_of_studyname === selectedFieldOfStudyName
+            );
+            setSelectedFieldOfStudy(selectedFieldOfStudy?._id);
+            // setSelectedSemester("")
+          }}
         />
       </div>
       <div className="flex flex-wrap  py-5  md:py-10 gap-5 md:gap-10">
-        <ProgramCard
-          title="Computer Science Engineering"
-          abbreviation="CSE"
-          programName="Btech"
-          semestersCount={8}
-          coursesCount={40}
-          fetchCourseDetails
-          openModal={openModal}
-        />
-        <ProgramCard
-          title="Electronics and Communication Engineering"
-          abbreviation="ECE"
-          programName="Btech"
-          semestersCount={8}
-          coursesCount={40}
-          fetchCourseDetails
-        />
-        <ProgramCard
-          title="Mechanical Engineering"
-          abbreviation="MECH"
-          programName="Btech"
-          semestersCount={8}
-          coursesCount={40}
-          fetchCourseDetails
-        />
-        <ProgramCard
-          title="Smart Manufacturing"
-          abbreviation="SM"
-          programName="Btech"
-          semestersCount={8}
-          coursesCount={40}
-          fetchCourseDetails
-        />
-        <ProgramCard
-          title="Design"
-          abbreviation="DES"
-          programName="BDES"
-          semestersCount={6}
-          coursesCount={40}
-          fetchCourseDetails
-        />
+        
+        {fieldOfStudy.map((fos, index) => (
+          <FieldOfStudyCard
+            key={index}
+            title={fos.field_of_studyfullname}
+            abbreviation={fos.field_of_studyname}
+            programName={selectedProgramDetails.program_name}
+            semestersCount={selectedProgramDetails.no_of_semester}
+            coursesCount={courseCount}
+            onEdit={() => openEditfosModal(fos)}
+            onDelete={() => {
+              setCurrentFieldOfStudy(fos);
+              openDeleteModal();
+            }}
+          />
+        ))}
       </div>
       {/* Modal */}
       <Modal
         //  style={{ zIndex: 9999 }}
-        open={modalOpen}
+        open={openModal}
         // onClose={closeModal}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
@@ -222,10 +332,17 @@ const AdminCourseNavbarFieldOfStudyComponent = () => {
               >
                 Select Program*
               </label>
-              <Dropdown2
+              <Dropdown
                 name="Program"
-                options={programs}
-                onSelect={setFormSelectedProgram}
+                options={programs?.map((program) => program?.program_name)}
+                onSelect={(selectedProgramName) => {
+                  const selectedProgram = programs?.find(
+                    (program) => program?.program_name === selectedProgramName
+                  );
+                  setSelectedProgram(selectedProgram?._id);
+                  // setSelectedSemester("")
+                  setSelectedFieldOfStudy("")
+                }}
               />
             </div>
 
@@ -238,8 +355,8 @@ const AdminCourseNavbarFieldOfStudyComponent = () => {
             <input
               id="fosname"
               type="text"
-              value={fosDetails.fieldsOfStudy_name}
-              onChange={(e) => setFosDetails(e.target.value)}
+              value={fieldOfStudy.fieldsOfStudy_name}
+              onChange={(e) => setFieldOfStudy(e.target.value)}
               placeholder="CSE"
               className="flex items-center w-full px-2 py-2 md:px-5 md:py-3 mr-2 text-sm
                 lg:text-[16px] font-medium outline-none focus:border-black mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-md border border-gray-300 "
@@ -255,8 +372,8 @@ const AdminCourseNavbarFieldOfStudyComponent = () => {
             <input
               id="fullfosname"
               type="text"
-              value={fosDetails.fieldsOfStudy_name}
-              onChange={(e) => setFosDetails(e.target.value)}
+              value={fieldOfStudy.fieldsOfStudy_name}
+              onChange={(e) => setFieldOfStudy(e.target.value)}
               placeholder="Computer Science Engineering"
               className="flex items-center w-full px-2 py-2 md:px-5 md:py-3 mr-2 text-sm lg:text-[16px] font-medium outline-none focus:border-black mb-7 placeholder:text-grey-700 bg-grey-200 text-dark-grey-900 rounded-md border border-gray-300 "
               required
@@ -269,25 +386,57 @@ const AdminCourseNavbarFieldOfStudyComponent = () => {
           </div>
         </Box>
       </Modal>
+
+      {/* Delete confirmation modal */}
+      <Modal
+        open={deleteModalOpen}
+        onClose={closeDeleteModal}
+        aria-labelledby="delete-modal-title"
+        aria-describedby="delete-modal-description"
+      >
+        <Box sx={style}>
+          <h3 className="text-lg font-semibold text-center mb-5">
+            Confirm Deletion
+          </h3>
+          <p className="text-center text-sm text-gray-600">
+            Are you sure you want to delete this program?
+          </p>
+          <div className="flex justify-center mt-5 ">
+            <Button
+              onClick={handleDeleteFieldOfStudy}
+              className="text-white bg-red-500"
+              variant="contained"
+
+            >
+              Delete
+            </Button>
+            <Button
+              onClick={closeDeleteModal}
+              className="text-black "
+              variant="outlined"
+              style={{ marginLeft: "1rem" }}
+            >
+              Cancel
+            </Button>
+          </div>
+        </Box>
+      </Modal>
     </div>
   );
 };
 
 export default AdminCourseNavbarFieldOfStudyComponent;
 
-const ProgramCard = ({
+const FieldOfStudyCard = ({
   title,
   abbreviation,
   programName,
   semestersCount,
   coursesCount,
-  fetchCourseDetails,
-  openModal,
+  onEdit,
+  onDelete,
 }) => {
-  const handleEdit = async () => {
-    await fetchCourseDetails(); // Fetch course details when modal opens
-    setModalOpen(true);
-  };
+  
   return (
     <Card className="w-full max-w-xs rounded-2xl border">
       <div className="flex flex-col h-full">
@@ -329,7 +478,7 @@ const ProgramCard = ({
             className="text-white bg-blue-800 "
             size="sm"
             variant="contained"
-            onClick={() => handleEdit()}
+            onClick={onEdit}
           >
             Edit
           </Button>
@@ -338,6 +487,7 @@ const ProgramCard = ({
             variant="outlined"
             size="sm"
             color="error"
+            onClick={onDelete}
           >
             Delete
           </Button>
